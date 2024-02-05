57040023cf238558912ddce3d350c4b55d6e3d46
NadyaEvdokimova <146958813+NadyaEvdokimova@users.noreply.github.com>
2024-02-05 13:24:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_HOLIDAY_URL = "https://holidays.abstractapi.com/v1/";
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.post("/search", async(req, res) => {
+    const country = req.body.country;
+    var day = req.body.day;
+    var month = req.body.month;
+    var year = req.body.year;
+    var DateTime = new Date();
+    if (day === "") {
+        day = DateTime.getDate();
+    }
+    if (month === "") {
+        month = DateTime.getMonth() + 1;
+    }
+    if (year === "") {
+        year = DateTime.getFullYear();
+    }
+    if (country === "error") {
+        console.log("Wrong country");
+    }
+
+    try {
+        const result = await axios.get(API_HOLIDAY_URL, {params : {
+            api_key : API_KEY,
+            country : country,
+            day : day,
+            month : month,
+            year : year
+        }
+        });
+        console.log(result.data);
+        res.render("index.ejs", { content: result.data });
+      } catch (error) {
+        console.log(error);
+      }
+
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
